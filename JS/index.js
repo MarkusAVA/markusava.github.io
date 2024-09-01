4d0bd23fd3dd048cd92e23e53ce094425c54ee92
Markus <48354544+MarkusAVA@users.noreply.github.com>
2024-09-01 18:59:14 +0200

    testing
    trying to add the header from a different html file so id can easily be imported into all files without added in the code each time a new page is created

--- JS/index.js
+++ JS/index.js
@@ -2,3 +2,9 @@ function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("show");
 }
+ // JavaScript to load the header
+document.getElementById("header-placeholder").innerHTML = fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("header-placeholder").innerHTML = data;
+});
