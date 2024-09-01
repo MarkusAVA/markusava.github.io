d0b327b3c95bcddf6a56a89484a54e8dc67605ac
Markus <48354544+MarkusAVA@users.noreply.github.com>
2024-09-01 19:08:31 +0200

    tests
    trying again

--- JS/index.js
+++ JS/index.js
@@ -2,9 +2,16 @@ function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("show");
 }
- // JavaScript to load the header
-document.getElementById("header-placeholder").innerHTML = fetch("header.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("header-placeholder").innerHTML = data;
+// JavaScript to load the header
+function loadHeader() {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        });
+}
+
+// Call the function to load the header when the page loads
+document.addEventListener("DOMContentLoaded", function() {
+    loadHeader();
 });
